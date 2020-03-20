aed0a123ac6150e65c25d3e42bdb5d5ea6ab6175
Jamie95187 <jamiewong63@googlemail.com>
2020-03-20 01:37:52 +0000

    fix axzios url path

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const instance axios.create({
-  baseURL: 'https://react-my-burger.firebaseio.com/'
+const instance = axios.create({
+  baseURL: 'https://react-my-buger-39b2c.firebaseio.com'
 });
 
 export default instance;
