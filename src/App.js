711b2009c3990e63e0d3f6ec4c6c7109cba398c5
Jamie95187 <jamiewong63@googlemail.com>
2020-04-21 23:09:19 +0100

    feat: building checkout container

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 import Layout from './containers/Layout/Layout';
 
 
 class App extends Component {
-  
+
   render(){
     return (
       <div className="App">
         <Layout>
           <BurgerBuilder />
+          <Checkout />
         </Layout>
       </div>
     );
