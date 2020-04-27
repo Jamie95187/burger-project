3aff9f935b76ba3e571f9bd22b5ad138ebd988f8
Jamie95187 <jamiewong63@googlemail.com>
2020-04-27 22:03:11 +0100

    combining reducers

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -187,9 +187,9 @@ class ContactData extends Component {
 
 const mapStateToProps = state => {
   return {
-    ings: state.ingredients,
-    price: state.totalPrice,
-    loading: state.loading
+    ings: state.burgerBuilder.ingredients,
+    price: state.burgerBuilder.totalPrice,
+    loading: state.order.loading
   }
 }
 
@@ -199,4 +199,4 @@ const dispatchToProps = dispatch => {
   };
 };
 
-export default connect(mapStateToProps)(withErrorHandler(ContactData, axios));
+export default connect(mapStateToProps, dispatchToProps)(withErrorHandler(ContactData, axios));
