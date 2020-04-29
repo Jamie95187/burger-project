cb452912748c9a898b1ce2e4bd12841a9d30568b
Jamie95187 <jamiewong63@googlemail.com>
2020-04-29 04:40:04 +0100

    add sign-in

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -21,7 +21,7 @@ export const authFail = (error) => {
   };
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
   return dispatch => {
     dispatch(authStart());
     const authData = {
@@ -29,14 +29,18 @@ export const auth = (email, password) => {
       password: password,
       returnSecureToken: true
     };
-    axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA1yTT4ZtpLg6KhCxwHGzRvXWwWock7t2c', authData)
+    let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA1yTT4ZtpLg6KhCxwHGzRvXWwWock7t2c';
+    if(!isSignup) {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA1yTT4ZtpLg6KhCxwHGzRvXWwWock7t2c';
+    }
+    axios.post(url, authData)
       .then(response => {
         console.log(response);
         dispatch(authSuccess(response.data));
       })
       .catch(err => {
         console.log(err);
-        dispatch(authFail());
+        dispatch(authFail(err));
       });
   };
 };
