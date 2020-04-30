d7c5017efb0ff96886d8e32cbbe467ae01dd6a97
Jamie95187 <jamiewong63@googlemail.com>
2020-04-30 23:41:06 +0100

    test: testing redux in auth reducer

---
+++ src/store/reducers/auth.test.js
@@ -0,0 +1,38 @@
+// Here we don't need enzyme we are just testing normal JavaScript code. We can use Jest no need for enzyme.
+// Simple input to output testing
+
+import reducer from './auth';
+import * as actionTypes from '../actions/actionTypes';
+
+describe('auth reducer', () => {
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual({
+      token: null,
+      userId: null,
+      error: null,
+      loading: false,
+      authRedirectPath: '/'
+    });
+  });
+
+  it('should store the token upon login', () => {
+    expect(reducer({
+      token: null,
+      userId: null,
+      error: null,
+      loading: false,
+      authRedirectPath: '/'
+    }, {
+      type: actionTypes.AUTH_SUCCESS,
+      idToken: 'some-token',
+      userId: 'some-user-id'
+    })).toEqual({
+      token: 'some-token',
+      userId: 'some-user-id',
+      error: null,
+      loading: false,
+      authRedirectPath: '/'
+    });
+  })
+  
+})
